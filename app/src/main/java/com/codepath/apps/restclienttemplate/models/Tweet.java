95a69075e7b1db11f22a108238f8d8fe0feac3ff
Thieusike-Pro\Thieusike <Tam101901@gmail.com>
2017-07-03 07:11:29 +0700

    commit basic get Twitter API

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -0,0 +1,70 @@
+package com.codepath.apps.restclienttemplate.models;
+
+
+
+import com.codepath.apps.restclienttemplate.MyDatabase;
+import com.raizlabs.android.dbflow.annotation.Column;
+import com.raizlabs.android.dbflow.annotation.PrimaryKey;
+import com.raizlabs.android.dbflow.annotation.Table;
+import com.raizlabs.android.dbflow.structure.BaseModel;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Thieusike on 7/2/2017.
+ */
+@Table(database = MyDatabase.class)
+public class Tweet extends BaseModel {
+    // Define database columns and associated fields
+    @PrimaryKey
+    @Column
+    Long id;
+    @Column
+    String userId;
+    @Column
+    String userHandle;
+    @Column
+    String timestamp;
+    @Column
+    String body;
+
+    public Tweet() {
+    }
+
+    public Tweet(JSONObject object) {
+        super();
+
+        try {
+            this.userId = object.getString("user_id");
+            this.userHandle = object.getString("user_username");
+            this.timestamp = object.getString("timestamp");
+            this.body = object.getString("body");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList<Tweet> fromJson(JSONArray jsonArray) {
+        ArrayList<Tweet> tweets = new ArrayList<Tweet>(jsonArray.length());
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject tweetJson = null;
+            try {
+                tweetJson = jsonArray.getJSONObject(i);
+            } catch (Exception e) {
+                e.printStackTrace();
+                continue;
+            }
+
+            Tweet tweet = new Tweet(tweetJson);
+            tweet.save();
+            tweets.add(tweet);
+        }
+
+        return tweets;
+    }
+}
